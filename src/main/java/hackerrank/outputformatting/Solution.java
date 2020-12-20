dc226d23181f853e2b3290ea23b8f71d736c70a9
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2020-12-20 20:35:42 +0100

    Committed the 5th solution.

---
+++ src/main/java/hackerrank/outputformatting/Solution.java
@@ -0,0 +1,26 @@
+package hackerrank.outputformatting;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            System.out.printf(s1);
+            for(int j= s1.length(); j<=15;j++){
+                System.out.printf(" ");
+            }
+            if (Integer.toString(x).length() < 3){
+                System.out.printf("0");
+            }
+            System.out.println(x);
+        }
+        System.out.println("================================");
+    }
+}
+
